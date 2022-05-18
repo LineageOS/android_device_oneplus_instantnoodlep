2ef8e7fb066c354e7b0190799e5617d577b80771
Alexander Koskovich <akoskovich@pm.me>
2022-05-18 09:03:29 -0700

    instantnoodlep: Alphabetically sort makefiles
    Change-Id: Ie31e1c256e096f578b27f04a0087af46ff0c4b39

--- device.mk
+++ device.mk
@@ -4,6 +4,24 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# AAPT
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
+
+# Audio
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 3168
+TARGET_SCREEN_WIDTH := 1440
+
+# Device init scripts
+PRODUCT_PACKAGES += \
+    fstab.qcom \
+    fstab.qcom.ramdisk \
+    init.recovery.target.rc
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
@@ -13,36 +31,19 @@ PRODUCT_PACKAGES += \
     OPlusSettingsProviderResTarget \
     OPlusSystemUIResTarget
 
-# Device uses high-density artwork where available
-PRODUCT_AAPT_CONFIG := normal
-PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
-
-# Boot animation
-TARGET_SCREEN_HEIGHT := 3168
-TARGET_SCREEN_WIDTH := 1440
-
-# Audio
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
-
-# Device init scripts
-PRODUCT_PACKAGES += \
-    fstab.qcom \
-    fstab.qcom.ramdisk \
-    init.recovery.target.rc
-
 # PowerShare
 PRODUCT_PACKAGES += \
     vendor.lineage.powershare@1.0-service.oneplus
+
+# Shipping API
+PRODUCT_SHIPPING_API_LEVEL := 29
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
-PRODUCT_SHIPPING_API_LEVEL := 29
-
-# Inherit from oneplus sm8250-common
+# Inherit from the common OEM chipset makefile.
 $(call inherit-product, device/oneplus/sm8250-common/common.mk)
 
-# Inherit from vendor blobs
+# Inherit from the proprietary files makefile.
 $(call inherit-product, vendor/oneplus/instantnoodlep/instantnoodlep-vendor.mk)
