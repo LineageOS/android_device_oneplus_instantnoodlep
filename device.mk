2fc478f161075c6f2d638dd3b66e3440aa89b27d
ZVNexus <akoskovich@pm.me>
2022-06-25 16:31:05 -0700

    instantnoodlep: overlay: Convert to RRO packages
    Change-Id: Iae6ef9fa9743bd2447991ad11d38fa184b12981d

--- device.mk
+++ device.mk
@@ -24,8 +24,12 @@
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
+
+PRODUCT_PACKAGES += \
+    OPlusFrameworksResTarget \
+    OPlusSettingsProviderResTarget \
+    OPlusSystemUIResTarget
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
@@ -50,6 +54,10 @@ PRODUCT_PACKAGES += \
 # PowerShare
 PRODUCT_PACKAGES += \
     vendor.lineage.powershare@1.0-service.oplus
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
 
 PRODUCT_SHIPPING_API_LEVEL := 29
 
