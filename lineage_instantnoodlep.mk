7aa3f16efdfd1b7b7c36d21b4717133f01b01e0e
LuK1337 <priv.luk@gmail.com>
2023-05-04 17:36:08 +0200

    instantnoodlep: Update from OOS 11.F.64
    Change-Id: I992acdda4639c1f9e4c07c3053e4b66eabcb309f

--- lineage_instantnoodlep.mk
+++ lineage_instantnoodlep.mk
@@ -26,8 +26,8 @@ PRODUCT_SYSTEM_DEVICE := OnePlus8Pro
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="OnePlus8Pro-user 13 RKQ1.211119.001 Q.ed8bd1_1_2 release-keys" \
+    PRIVATE_BUILD_DESC="OnePlus8Pro-user 13 RKQ1.211119.001 Q.f9c4de_1_5b9c release-keys" \
     TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
     TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
 
-BUILD_FINGERPRINT := OnePlus/OnePlus8Pro/OnePlus8Pro:13/RKQ1.211119.001/Q.ed8bd1_1_2:user/release-keys
+BUILD_FINGERPRINT := OnePlus/OnePlus8Pro/OnePlus8Pro:13/RKQ1.211119.001/Q.f9c4de_1_5b9c:user/release-keys
