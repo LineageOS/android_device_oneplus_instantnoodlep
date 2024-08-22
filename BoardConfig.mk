d217809898ed0da355f5ed446ab4b6f8a241bd05
LuK1337 <priv.luk@gmail.com>
2024-08-22 12:55:51 +0200

    instantnoodlep: Switch to common PowerShare HAL
    Change-Id: I880f7797849070d1dc928141e2dc3a6b3ca2cc12

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,6 +15,10 @@ TARGET_SCREEN_DENSITY := 450
 # HIDL
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 
+# PowerShare
+TARGET_POWERSHARE_DISABLED := disable
+TARGET_POWERSHARE_PATH := /proc/wireless/enable_tx
+
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
