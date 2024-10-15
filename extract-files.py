e2a5aca58688e967c3f67594445d3cc160bff7be
LuK1337 <priv.luk@gmail.com>
2024-10-15 22:11:44 +0200

    eepy
    Change-Id: I5618bf10c107df579a540e2393b222128961e670

---
+++ extract-files.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+blob_fixups: blob_fixups_user_type = {
+    'odm/etc/init/wlchgmonitor.rc': blob_fixup()
+        .regex_replace('\tdisabled\n', '')
+        .regex_replace('\tseclabel u:r:wlchgmonitor:s0\n', ''),
+}  # fmt: skip
+
+module = ExtractUtilsModule(
+    'instantnoodlep',
+    'oneplus',
+    blob_fixups=blob_fixups,
+    add_firmware_proprietary_file=True,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device_with_common(
+        module, 'sm8250-common', module.vendor
+    )
+    utils.run()
