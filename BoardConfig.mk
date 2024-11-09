16b78c8429d93167440119894b974a95ba9a9973
pjgowtham <pjgowtham@gmail.com>
2024-11-09 10:31:18 +0530

    Revert "instantnoodlep: We now run at 1080p by default ^.^"
    This reverts commit 0f6179163c1765984864bea484a5efd35cd8bf7c.
    
    Change-Id: I349159718e74b8231e7ede2cf6d4b4f0bc31cd0f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,9 +9,6 @@ include device/oneplus/sm8250-common/BoardConfigCommon.mk
 
 DEVICE_PATH := device/oneplus/instantnoodlep
 
-# Display
-TARGET_SCREEN_DENSITY := 450
-
 # HIDL
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 
@@ -23,6 +20,7 @@ BOARD_ONEPLUS_DYNAMIC_PARTITIONS_SIZE := 7511998464
 BOARD_SUPER_PARTITION_SIZE := 15032385536
 
 # Recovery
+TARGET_RECOVERY_DENSITY := xxhdpi
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/init/fstab.qcom
 TARGET_RECOVERY_UI_MARGIN_HEIGHT := 103
 
