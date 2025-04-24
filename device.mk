dbbedb2de58975a3f6b568a2b44fab3c98d068ec
Bruno Martins <bgcngm@gmail.com>
2025-04-24 21:36:24 +0100

    instantnoodlep: Switch to PowerShare AIDL
    Change-Id: I97400638493a3b9d5a3b87fa6831c9affc7c3def

--- device.mk
+++ device.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2018 The LineageOS Project
+# Copyright (C) 2018-2025 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -44,7 +44,7 @@ PRODUCT_PACKAGES += \
 
 # PowerShare
 PRODUCT_PACKAGES += \
-    vendor.lineage.powershare@1.0-service.oplus
+    vendor.lineage.powershare-service.oplus
 
 # Shipping API
 PRODUCT_SHIPPING_API_LEVEL := 29
